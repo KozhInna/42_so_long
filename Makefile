23baff9083c5244c2c1d2675ba2fee5a992af442
Inna Kozhina <inna.kozhina22@gmail.com>
2025-03-21 15:26:54 +0200

    add makefile

---
+++ Makefile
@@ -0,0 +1,47 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    Makefile                                           :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: ikozhina <ikozhina@student.hive.fi>        +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2025/03/21 14:44:32 by ikozhina          #+#    #+#              #
+#    Updated: 2025/03/21 15:08:15 by ikozhina         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+NAME = so_long
+
+CC = cc
+
+FLAGS = -Werror -Wextra -Wall -I./libft -I./include
+
+SRCS = src/so_long_main.c
+
+OBJS = $(SRCS:.c=.o)
+
+LIBFT = libft/libft.a
+LIBFT_PATH = libft
+
+all: $(LIBFT) $(NAME)
+
+$(NAME): $(OBJS) $(LIBFT)
+	$(CC) $(FLAGS) $(OBJS) $(LIBFT) -o $(NAME)
+
+$(LIBFT):
+	$(MAKE) -C $(LIBFT_PATH)
+
+%.o: %.c
+	$(CC) $(FLAGS) -c $< -o $@
+
+clean:
+	rm -f $(OBJS)
+	$(MAKE) -C $(LIBFT_PATH) clean
+
+fclean: clean
+	rm -f $(NAME)
+	$(MAKE) -C $(LIBFT_PATH) fclean
+
+re:	fclean all
+
+.PHONY: all clean fclean re
--- src/so_long_main.c
+++ src/so_long_main.c
@@ -1,46 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   so_long_main.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ikozhina <ikozhina@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/21 10:51:03 by ikozhina          #+#    #+#             */
-/*   Updated: 2025/03/21 11:47:17 by ikozhina         ###   ########.fr       */
+/*   Updated: 2025/03/21 15:07:20 by ikozhina         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include<stdio.h>
-
-int	ft_strcmp(const char *s1, const char *s2)
-{
-	size_t	i;
-
-	i = 0;
-	while ((unsigned char)s1[i] != '\0' && (unsigned char)s2[i] != '\0')
-	{
-		if ((unsigned char)s1[i] != (unsigned char)s2[i])
-			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
-		i++;
-	}
-	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
-}
-
-char	*ft_strrchr(const char *s, int c)
-{
-	char	*pnt_s;
-
-	pnt_s = NULL;
-	while (*s)
-	{
-		if ((unsigned char)*s == (unsigned char)c)
-			pnt_s = (char *)s;
-		s++;
-	}
-	if (c == '\0')
-		return ((char *)s);
-	return (pnt_s);
-}
+#include "so_long.h"
 
 void	check_extention(char *file_name)
 {
@@ -66,5 +36,5 @@ int main(int argc, char **argv)
 {
 	if (argc == 2)
 		printf("%s - ", argv[1]);
-	check_argument(argv[1]);
+	check_extention(argv[1]);
 }
