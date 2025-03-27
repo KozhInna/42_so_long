7cd1356c4db450ec0e12e339b0759aa2217c1e51
KozhInna <inna.kozhina22@gmail.com>
2025-03-27 10:48:15 +0200

    wall validation

--- src/validate_map.c
+++ src/validate_map.c
@@ -6,27 +6,11 @@
 /*   By: ikozhina <ikozhina@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/24 12:32:19 by ikozhina          #+#    #+#             */
-/*   Updated: 2025/03/26 22:49:54 by ikozhina         ###   ########.fr       */
+/*   Updated: 2025/03/27 10:37:32 by ikozhina         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
-
-int	is_ractangular(t_map *map)
-{
-	size_t i;
-
-	map->columns = ft_strlen(map->map_data[0]);
-	i = 1;
-	while (i < map->rows)
-	{
-		if (map->columns != ft_strlen(map->map_data[i++]))
-			return (1);
-	}
-	if (map->rows == map->columns)
-		return (1);
-	return (0);
-}
 
 void	safe_exit(t_map *map)
 {
@@ -42,6 +26,54 @@ void	safe_exit(t_map *map)
 	exit(1);
 }
 
+void	is_ractangular(t_map *map)
+{
+	size_t i;
+
+	map->columns = ft_strlen(map->map_data[0]);
+	i = 1;
+	while (i < map->rows)
+	{
+		if (map->columns != ft_strlen(map->map_data[i++]))
+		{
+			ft_putstr_fd("Error\nMap must be rectangular\n", 2);
+			safe_exit(map);
+		}
+	}
+	if (map->rows == map->columns)
+	{
+		ft_putstr_fd("Error\nMap must be rectangular\n", 2);
+		safe_exit(map);
+	}
+}
+void	check_walls(t_map *map)
+{
+	size_t i;
+	size_t j;
+
+	i = 0;
+	while (i < map->rows)
+	{
+		j = 0;
+		while (map->map_data[i][j])
+		{
+			if ((i == 0 || i == map->rows - 1) && map->map_data[i][j] != '1')
+			{
+				ft_putstr_fd("Error\nMap must be surrounded by walls\n", 2);
+				safe_exit(map);
+			}
+			if ((j == 0 || j == map->columns - 1) && map->map_data[i][j] != '1')
+			{
+				ft_putstr_fd("Error\nMap must be surrounded by walls\n", 2);
+				safe_exit(map);
+			}
+			j++;
+		}
+		i++;
+	}
+}
+
+
 int	validate_map(char *map_file)
 {
 	t_map *map;
@@ -49,10 +81,8 @@ int	validate_map(char *map_file)
 	map = parse_map(map_file);
 	if (!map)
 		safe_exit(map);
-	if (is_ractangular(map) != 0)
-	{
-		ft_putstr_fd("Error\nMap must be rectangular\n", 2);
-		safe_exit(map);
-	}
+	is_ractangular(map);
+	check_walls(map);
+	printf("valid map\n");
 	return (1);
 }
