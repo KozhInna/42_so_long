94ebebc8ff13924873802369e0b4ffa07ecdb93e
KozhInna <inna.kozhina22@gmail.com>
2025-04-05 13:34:09 +0300

    simplify handle_move logic by extracting player and collectible updates

--- src/handle_moves.c
+++ src/handle_moves.c
@@ -6,58 +6,59 @@
 /*   By: ikozhina <ikozhina@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/31 13:25:43 by ikozhina          #+#    #+#             */
-/*   Updated: 2025/04/04 13:06:19 by ikozhina         ###   ########.fr       */
+/*   Updated: 2025/04/05 13:28:19 by ikozhina         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include"so_long.h"
 
-void handle_move(t_game *game, int new_x, int new_y)
+void	update_player_position(t_game *game, int new_x, int new_y)
 {
 	int x;
 	int y;
-	size_t	i;
 
 	x = game->map->player.x;
 	y = game->map->player.y;
-	if (game->map->map_data[new_y][new_x] == '1')
-		return ;
-	if (game->map->map_data[new_y][new_x] == '0' ||
-		game->map->map_data[new_y][new_x] == 'C')
+	game->map->map_data[y][x] = '0';
+	game->map->map_data[new_y][new_x] = 'P';
+	game->map->player.x = new_x;
+	game->map->player.y = new_y;
+	game->img_player->instances[0].x = new_x * T_SIZE;
+	game->img_player->instances[0].y = new_y * T_SIZE;
+	game->move_count += 1;
+	ft_printf("Current move: %d\n", game->move_count);
+}
+void	update_collectible(t_game *game, int new_x, int new_y)
+{
+	size_t	i;
+	
+	i = 0;
+	while (i < game->img_collectible->count)
 	{
-		if (game->map->map_data[new_y][new_x] == 'C')
-		{
-			i = 0;
-			while (i < game->img_collectible->count)
-			{
-				if (game->img_collectible->instances[i].x == new_x * T_SIZE &&
-					game->img_collectible->instances[i].y == new_y * T_SIZE)
-					game->img_collectible->instances[i].enabled = false;
-				i++;
-			}
-			game->map->collectibles--;
-		}
-		game->map->map_data[y][x] = '0';
-		game->map->map_data[new_y][new_x] = 'P';
-		game->map->player.x = new_x;
-		game->map->player.y = new_y;
-		game->img_player->instances[0].x = new_x * T_SIZE;
-		game->img_player->instances[0].y = new_y * T_SIZE;
-		game->move_count += 1;
-		ft_printf("Current move: %d\n", game->move_count);
+		if (game->img_collectible->instances[i].x == new_x * T_SIZE &&
+			game->img_collectible->instances[i].y == new_y * T_SIZE)
+			game->img_collectible->instances[i].enabled = false;
+		i++;
 	}
-	if (game->map->map_data[new_y][new_x] == 'E')
-	{
-		if (game->map->collectibles == 0)
-		{
-			safe_exit(game->map);
-			ft_putstr_fd("Congratulations! You won!\n", 1);
-			exit(0);
-		}
+	game->map->collectibles--;
+}
+void handle_move(t_game *game, int new_x, int new_y)
+{
+	char tile;
+
+	tile = game->map->map_data[new_y][new_x];
+	if (tile == '1')
 		return ;
+	if (tile == 'C')
+		update_collectible(game, new_x, new_y);
+	if (tile == '0' || tile == 'C')
+		update_player_position(game, new_x, new_y);
+	if (tile == 'E' && game->map->collectibles == 0)
+	{
+		safe_exit(game->map);
+		ft_putstr_fd("Congratulations! You won!\n", 1);
+		exit(0);
 	}
-	x = game->map->player.x;
-	y = game->map->player.y;
 }
 
 void	close_game(void *parameter)
@@ -80,7 +81,6 @@ void	handle_click(mlx_key_data_t keydata, void *parameter)
 	game = (t_game *)parameter;
 	x = game->map->player.x;
 	y = game->map->player.y;
-	// printf("x - %d, y -%d\n", x, y);
 	if (keydata.key == MLX_KEY_ESCAPE && keydata.action == MLX_PRESS)
 		close_game(game);
 	if (keydata.key == MLX_KEY_UP && keydata.action == MLX_PRESS)
@@ -92,3 +92,4 @@ void	handle_click(mlx_key_data_t keydata, void *parameter)
 	if (keydata.key == MLX_KEY_RIGHT && keydata.action == MLX_PRESS)
 		handle_move(game, x + 1, y);
 }
+
