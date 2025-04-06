b9284d62cd1af6c62ce8a9788f03864653829b9b
KozhInna <inna.kozhina22@gmail.com>
2025-04-06 11:53:20 +0300

    get_next_line refactor buffer has predefined size not dynamically allocated

--- libft/get_next_line.c
+++ libft/get_next_line.c
@@ -6,19 +6,19 @@
 /*   By: ikozhina <ikozhina@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/09 09:58:14 by ikozhina          #+#    #+#             */
-/*   Updated: 2025/03/24 10:56:34 by ikozhina         ###   ########.fr       */
+/*   Updated: 2025/04/06 11:47:37 by ikozhina         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include"libft.h"
 
-static char	*handle_buffer(char **buffer, char *line);
-static int	read_to_buffer(int fd, char **buffer, char **line);
+static char	*handle_buffer(char *buffer, char *line);
+static int	read_to_buffer(int fd, char *buffer, char **line);
 static char	*ft_strjoin_up_nl(char const *s1, char const *s2);
 
 char	*get_next_line(int fd)
 {
-	static char	*buffer;
+	static char	buffer[BUFFER_SIZE];
 	char		*line;
 	int			status;
 
@@ -27,13 +27,13 @@ char	*get_next_line(int fd)
 	line = NULL;
 	while (1)
 	{
-		if (buffer && *buffer)
+		if (buffer[0] != '\0')
 		{
-			line = handle_buffer(&buffer, line);
+			line = handle_buffer(buffer, line);
 			if (!line || ft_strchr(line, '\n'))
 				return (line);
 		}
-		status = read_to_buffer(fd, &buffer, &line);
+		status = read_to_buffer(fd, buffer, &line);
 		if (status == 1)
 			return (line);
 		if (status == 0)
@@ -41,47 +41,39 @@ char	*get_next_line(int fd)
 	}
 }
 
-static char	*handle_buffer(char **buffer, char *line)
+static char	*handle_buffer(char *buffer, char *line)
 {
 	char	*ptr_newline;
 	char	*temp;
 
 	temp = line;
-	line = ft_strjoin_up_nl(temp, *buffer);
+	line = ft_strjoin_up_nl(temp, buffer);
 	free(temp);
 	if (!line)
 		return (NULL);
-	ptr_newline = ft_strchr(*buffer, '\n');
+	ptr_newline = ft_strchr(buffer, '\n');
 	if (ptr_newline)
-		ft_memmove(*buffer, ptr_newline + 1, ft_strlen(ptr_newline));
+		ft_memmove(buffer, ptr_newline + 1, ft_strlen(ptr_newline));
 	else
-		(*buffer)[0] = '\0';
+		buffer[0] = '\0';
 	return (line);
 }
 
-static int	read_to_buffer(int fd, char **buffer, char **line)
+static int	read_to_buffer(int fd, char *buffer, char **line)
 {
 	ssize_t	bytes_read;
 
-	if (!*buffer)
-	{
-		*buffer = malloc(BUFFER_SIZE + 1);
-		if (!*buffer)
-			return (0);
-		(*buffer)[0] = '\0';
-	}
-	bytes_read = read(fd, *buffer, BUFFER_SIZE);
+	bytes_read = read(fd, buffer, BUFFER_SIZE);
 	if (bytes_read <= 0)
 	{
-		free(*buffer);
-		*buffer = NULL;
+		buffer[0] = '\0';
 		if (bytes_read == 0 && *line && **line)
 			return (1);
 		free(*line);
 		*line = NULL;
 		return (0);
 	}
-	(*buffer)[bytes_read] = '\0';
+	buffer[bytes_read] = '\0';
 	return (2);
 }
 
